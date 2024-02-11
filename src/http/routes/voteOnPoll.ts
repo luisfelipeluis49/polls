35dbe3e764b90e1a9178b16ccea64ceda1a0d4ca
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2024-02-11 19:48:39 -0300

    aula 03

--- src/http/routes/voteOnPoll.ts
+++ src/http/routes/voteOnPoll.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'; 
 import { prisma } from '../../lib/prisma';
 import { FastifyInstance } from 'fastify';
+import { redis } from '../../lib/redis';
+import { voting } from '../../utils/voting-pub-sub';
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, response) => {
@@ -34,6 +36,14 @@ export async function voteOnPoll(app: FastifyInstance) {
                         id: userPreviousVoteOnPoll.id
                     }
                 })
+
+                const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(votes)
+                })
+
             } else if (userPreviousVoteOnPoll) {
                 return response.status(400).send({message: "You alreadt votede on this poll."})
             }
@@ -56,6 +66,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                 pollId,
                 pollOptionId
             }
+        })
+
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes)
         })
     
         return response.status(201).send({sessionId})
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
+import websocket from '@fastify/websocket';
 import { createPoll } from './routes/createPoll';
 import { getPoll } from './routes/getPoll';
 import { voteOnPoll } from './routes/voteOnPoll';
-import cookie from '@fastify/cookie';
+import { pollResults } from './websocket/poll-results';
 
 const app = fastify()
 
@@ -11,9 +13,13 @@ app.register(cookie, {
     hook: 'onRequest'
 })
 
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!');
